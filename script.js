ddb6ab2e1f4f9992409b3c8211e6232e615dc29c
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-19 16:43:10 +0500

    today practise

---
+++ script.js
@@ -0,0 +1,90 @@
+const mathNumber = prompt("What is Your Math Number")
+const englishNumber = prompt("What Is Your English Numbers")
+const urduNumber = prompt("What Is Your urdu Numbers")
+const islamiyateNumber = prompt("What Is Your islamiyate Numbers")
+const computerNumber = prompt("What Is Your computer Numbers")
+
+
+document.write("Your Math Number is  " + mathNumber + "<br>" + "Your English Number is " + englishNumber + "<br>" + "Your urdu Number is  " + urduNumber + "<br>" + "Your islamiyate Number is  " + islamiyateNumber + "<br>" + "Your computer Number is  " + computerNumber + "<br>")
+
+
+
+if (mathNumber > 33) {
+    document.write("Congratiols You are Pass in math Subject, And Your Number Is " + mathNumber + "<br>")
+} else {
+    document.write("You Are Fails in math Subject, And Your Number Is " + mathNumber + "<br>")
+}
+
+
+if (englishNumber > 33) {
+    document.write("Congratiols You are Pass in english Subject, And Your Number Is " + englishNumber + "<br>")
+} else {
+    document.write("You Are Fails in english Subject, And Your Number Is " + englishNumber + "<br>")
+}
+
+
+if (urduNumber > 33) {
+    document.write("Congratiols You are Pass in urdu Subject, And Your Number Is " + urduNumber + "<br>")
+} else {
+    document.write("You Are Fails in urdu Subject, And Your Number Is " + urduNumber + "<br>")
+}
+
+
+if (islamiyateNumber > 33) {
+    document.write("Congratiols You are Pass in islamiyate Subject, And Your Number Is " + islamiyateNumber + "<br>")
+} else {
+    document.write("You Are Fails in islamiyate Subject, And Your Number Is " + islamiyateNumber + "<br>")
+}
+
+
+if (computerNumber > 33) {
+    document.write("Congratiols You are Pass in computer Subject, And Your Number Is " + computerNumber + "<br>")
+} else {
+    document.write("You Are Fails in computer Subject, And Your Number Is " + computerNumber + "<br>")
+}
+
+
+
+// {
+//     let a = 10
+//     console.log(a); 
+// }
+
+
+// let a = 20
+// console.log(a);
+
+
+
+// const a = 10
+// a= 11
+
+// console.log(a);
+// a = null
+
+// console.log(typeof(a));
+
+
+// function tableOfTwo(multiplier) {
+//   if (multiplier > 10) {
+//     return;
+//   }
+//   document.write(`5 * ${multiplier} = ${5 * multiplier}<br>`);
+//   tableOfTwo(multiplier + 1);
+// }
+
+// tableOfTwo(1);
+
+// const students = [ "owais" , "shabaz" , "adnan"]
+
+// const pro = " Is pro Devloper"
+
+// console.log(students[2]+pro);
+
+// students.pop()
+
+// console.log(students);
+
+// students.push("newman", "feman", "heman")
+
+// console.log(students);
